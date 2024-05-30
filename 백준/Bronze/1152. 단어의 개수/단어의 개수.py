a792f5c83db20272164f1884216dd3823d0cec26
Nick <outgrowthr@gmail.com>
2024-05-30 15:38:21 +0900

    [Bronze II] Title: 단어의 개수, Time: 48 ms, Memory: 39160 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.py
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.py
@@ -1,8 +1,11 @@
-def count_words(input_str):  
-    return len(input_str.split()) # 단어의 개수를 반환  
-  
-# 사용자로부터 문자열 입력 받기  
-input_str = input()  
-  
-# 단어의 갯수 출력  
-print(count_words(input_str))
+# 단어의 개수
+# 접근방법: 문자열을 공백 기준으로 나누고, 배열의 요소를 카운트 한다. 
+import sys
+
+def count_words(input_string):
+    words = input_string.split()
+    word_count = len(words)
+    return word_count
+# len() 함수는 파이썬 내장 함수로, 객체(문자열, 리스트, 튜플 등)의 길이(또는 요소의 개수)를 반환한다.
+input_string = sys.stdin.read().strip()
+print(count_words(input_string))
